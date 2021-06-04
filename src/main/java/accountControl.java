bb4a879cf251fc0b2751c5944684a0d0a5bd5ff3
TonnieLamar <lloyanto@gmail.com>
2021-06-04 17:11:03 +0300

    Initial commit

---
+++ src/main/java/accountControl.java
@@ -0,0 +1,77 @@
+@Controller
+public class accountControl {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ConfirmationTokenRepository confirmationTokenRepository;
+
+    @Autowired
+    private EmailSenderService emailSenderService;
+
+    @RequestMapping(value="/register", method = RequestMethod.GET)
+    public ModelAndView displayRegistration(ModelAndView modelAndView, User user)
+    {
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("register");
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/register", method = RequestMethod.POST)
+    public ModelAndView registerUser(ModelAndView modelAndView, User user)
+    {
+
+        User existingUser = userRepository.findByEmailIdIgnoreCase(user.getEmailId());
+        if(existingUser != null)
+        {
+            modelAndView.addObject("message","This email already exists!");
+            modelAndView.setViewName("error");
+        }
+        else
+        {
+            userRepository.save(user);
+
+            ConfirmationToken confirmationToken = new ConfirmationToken(user);
+
+            confirmationTokenRepository.save(confirmationToken);
+
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setTo(user.getEmailId());
+            mailMessage.setSubject("Complete Registration!");
+            mailMessage.setFrom("ENTER SENDERS EMAIL ADDRESS, RECEPTION@TRACOM.CO.KE");
+            mailMessage.setText("To confirm your account, please click here : "
+                    +"http://localhost:ENTER LOCAL HOST/confirm-account?token="+confirmationToken.getConfirmationToken());
+
+            emailSenderService.sendEmail(mailMessage);
+
+            modelAndView.addObject("emailId", user.getEmailId());
+
+            modelAndView.setViewName("successfulRegisteration");
+        }
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/confirm-account", method= {RequestMethod.GET, RequestMethod.POST})
+    public ModelAndView confirmUserAccount(ModelAndView modelAndView, @RequestParam("token")String confirmationToken)
+    {
+        ConfirmationToken token = confirmationTokenRepository.findByConfirmationToken(confirmationToken);
+
+        if(token != null)
+        {
+            User user = userRepository.findByEmailIdIgnoreCase(token.getUser().getEmailId());
+            user.setEnabled(true);
+            userRepository.save(user);
+            modelAndView.setViewName("accountVerified");
+        }
+        else
+        {
+            modelAndView.addObject("message","The link is invalid or broken!");
+            modelAndView.setViewName("error");
+        }
+
+        return modelAndView;
+    }
+    // getters and setters
+}
